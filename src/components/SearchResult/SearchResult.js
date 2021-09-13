c3d10393b7771affe149fa5e6b7926b551fbf3ad
Ebuka Peter <peterdbrainy5@gmail.com>
2021-09-13 16:21:46 +0100

    SUBJECT: Add category and SearchResult components. BODY: Add category and SearchResult component. This will get data which are all the books available and display them

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './SearchResult.styles.css';
+
+
+class SearchResult extends Component {
+   
+    state = {
+
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+
+export default SearchResult;
---
+++ src/components/category/category.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './category.styles.css';
+
+
+class Category extends Component {
+   
+    state = {
+
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+
+export default Category;
